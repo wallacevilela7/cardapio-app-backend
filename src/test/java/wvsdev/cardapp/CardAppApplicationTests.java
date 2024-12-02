339028e3c401c97a3bb5fd51a95ccc53400d6e4a
Wallace Vilela <wallace.vilela9@gmail.com>
2024-12-02 18:32:40 -0300

    Create app

---
+++ src/test/java/wvsdev/cardapp/CardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.cardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
